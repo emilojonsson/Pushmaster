fc635764004f0711a1a8826371fbfb1e2d782247
emil <emil.o.jonsson@gmail.com>
2022-10-14 10:53:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pushmaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
